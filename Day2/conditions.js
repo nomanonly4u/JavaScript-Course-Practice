3cc5d36122c85ed8fc022dc07be178ba6d347ab5
nomanonly4u <nrauf2825@gmail.com>
2024-04-16 17:17:28 +0500

    added conditions

---
+++ Day2/conditions.js
@@ -0,0 +1,26 @@
+console.log("hello noman");
+
+let num = 5 , num2 = 10;
+console.log(num+num2);
+console.log(num*num2);
+console.log(num/num2*10);
+console.log(num**num2);
+console.log(num%num2);
+
+// if(num>20){
+//     console.log(`number is greater ${num}`);
+// }
+// else if(num = 10){
+//     console.log("num is "+" "+ num )
+// }
+// else {
+//     console.log("number is not greater" +" "+ num);
+// }
+
+// ternary operator
+let m = 6;
+let n = m>10 ? "hello" : "bye";
+console.log(n);
+
+
+
